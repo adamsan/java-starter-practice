f205020ba77422d057dafd0dc1eecdb41b9f2074
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-10-22 14:23:53 +0200

    add task [count surviving sheep by wolf attack]

--- src/com/codecool/codewars/SheepCount.java
+++ src/com/codecool/codewars/SheepCount.java
@@ -7,6 +7,13 @@ public class SheepCount {
 
         System.out.println("Sheeps counted: " + sheepCount);
         System.out.println("Wolfs counted: " + wolfCount);
+
+        // Print the sheep surviving the wolf attack:
+        // If a sheep is preceeded by a wolf, it will be eaten.
+        // Example:  orange sheep wolf sheep sheep
+        //                               ^ this sheep will be eaten by the wolf befor it.
+        // Output: Sheep survived: 2
+
     }
 
     private static int count(String target, String[] strings) {
