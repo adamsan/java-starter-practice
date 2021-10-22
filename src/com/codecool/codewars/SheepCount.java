47df791443f9ac6b38fc3580c43153b47c0c6207
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-10-22 14:19:34 +0200

    remove commented code, fix sheep-wolf strings

--- src/com/codecool/codewars/SheepCount.java
+++ src/com/codecool/codewars/SheepCount.java
@@ -2,18 +2,9 @@ package com.codecool.codewars;
 
 public class SheepCount {
     public static void main(String[] args) {
-        int sheepCount = count("wolf", args);
-        int wolfCount = count("sheep", args);
+        int sheepCount = count("sheep", args);
+        int wolfCount = count("wolf", args);
 
-//        int sheepCount = 0;
-//        int wolfCount = 0;
-//        for (String arg : args) {
-//            if ("sheep".equals(arg)) {
-//                sheepCount++;
-//            } else if ("wolf".equals(arg)) {
-//                wolfCount++;
-//            }
-//        }
         System.out.println("Sheeps counted: " + sheepCount);
         System.out.println("Wolfs counted: " + wolfCount);
     }
