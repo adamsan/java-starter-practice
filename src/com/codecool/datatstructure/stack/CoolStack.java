4d880677cab39a8276b628196c5cafe525fe4c4c
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-17 15:20:20 +0100

    solve CoolStack - dynamically growing

--- src/com/codecool/datatstructure/stack/CoolStack.java
+++ src/com/codecool/datatstructure/stack/CoolStack.java
@@ -5,14 +5,29 @@ package com.codecool.datatstructure.stack;
  * Internally use an array to store the values, don't use collections.
  */
 public class CoolStack {
+    int[] values = new int[3];
+    int size = 0;
+
     public void push(int i) {
+        if (size == values.length) {
+            grow();
+        }
+        values[size++] = i;
+    }
+
+    private void grow() {
+        int[] newValues = new int[values.length * 2];
+        for (int j = 0; j < values.length; j++) {
+            newValues[j] = values[j];
+        }
+        values = newValues;
     }
 
     public int pop() {
-        return 0;
+        return values[--size];
     }
 
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 }
