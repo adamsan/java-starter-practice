e6c6495145486cb27fca7adfba902fdeefc49a22
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-17 01:58:14 +0100

    solve isProperlyNested for simple case.

--- src/com/codecool/datatstructure/SimpleParensNestingChecker.java
+++ src/com/codecool/datatstructure/SimpleParensNestingChecker.java
@@ -20,6 +20,18 @@ public class SimpleParensNestingChecker {
      * Throw IllegalArgumentException, if input is invalid (contains other characters, than parens).
      */
     private static boolean isProperlyNested(String str) {
-        return false;
+        for (char ch : str.toCharArray()) {
+            if (ch != '(' && ch != ')') throw new IllegalArgumentException("Invalid input!");
+        }
+        int open = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
+                open++;
+            } else if (ch == ')') {
+                open--;
+                if (open < 0) return false;
+            }
+        }
+        return open == 0;
     }
 }
