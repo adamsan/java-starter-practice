0266e86b13bf1f3278f4e736a6a31d1999c9a2c4
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 11:43:08 +0100

    add OOP drinks task start

---
+++ src/com/codecool/oop/drinks/App.java
@@ -0,0 +1,32 @@
+package com.codecool.oop.drinks;
+
+/**
+ * Use OOP to model the following drinks menu:
+ *
+ * Drinks [name, price, quantity, toString]
+ *     Alcoholic
+ *         Shot
+ *             - Whiskey 2cl 1200 Ft
+ *         Beer
+ *             - Soproni Ászok 0,5L 450 Ft
+ *         Wine
+ *             - Chardonnay 0,75l 515 Ft/dl
+ *         Cocktail
+ *             - Piña Colada 0,75l 1750 Ft
+ *                 összetevők: Fehér rum, kókuszszirup, tejszín, ananászlé
+ *     Non-Alcoholic
+ *         Water
+ *             - Theodora szénsavmentes 0,33l 500 Ft
+ *         Soft Drink
+ *             - Sprite 5dl 800 Ft
+ *             - Fanta 5dl 800 Ft
+ *         Coffee
+ *             - Cappuccino 590 Ft
+ *             - Caffe Latte 590 Ft
+ *         Tee
+ *             - Házi jeges tea 400ml 900 FT
+ *             - Fekete tea 600ml 950 FT
+ *             - Egzotikus gyümölcs tea 1000ml 1200 FT
+ */
+public class App {
+}
