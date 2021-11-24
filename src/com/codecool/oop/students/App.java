99f4dd67c45a7fc9f3e9df63f577fcdc0f7fffcd
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 17:51:21 +0100

    add OOP task students - Comparable, Comparator, sort

---
+++ src/com/codecool/oop/students/App.java
@@ -0,0 +1,17 @@
+package com.codecool.oop.students;
+
+/**
+ * Task:
+ * - Modell a Student class with name and age.
+ * - Create a StudentDAO interface, with a method to find all students
+ * - Create a simple, in-memory implementation for it.
+ * - In main, query the students in a list.
+ * - Print all student - print should display the student's name, and age.
+ * - Order the students - default ordering should be ascending by age,
+ * students with the same age should be ordered by name. HINT: use Comparable interface
+ * - Order the students - descending by age. HINT: create a custom Comparator
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+}
