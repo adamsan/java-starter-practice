21644e372cd889f6ad2cf6bf12da92ca0697cb42
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 11:37:35 +0100

    solve recursive fibonacchi task

--- src/com/codecool/recursion/Fibonacchi.java
+++ src/com/codecool/recursion/Fibonacchi.java
@@ -16,6 +16,8 @@ public class Fibonacchi {
      * fibo(n) = fibo(n-1)+fibo(n-2)
      */
     private static int fibo(int i) {
-        return 0;
+        if (i == 0) return 0;
+        if (i == 1) return 1;
+        return fibo(i - 1) + fibo(i - 2);
     }
 }
