bc9369e5ebc4fae8f1764d297cc4e684f1b0c53e
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 11:28:54 +0100

    add task recursive factorial calculaton

---
+++ src/com/codecool/recursion/Factorial.java
@@ -0,0 +1,18 @@
+package com.codecool.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(fact(i));
+        }
+    }
+
+    /**
+     * Calculates the i-th factorial
+     * fact(0) = 1
+     * fact(n) = n * fact(n-1)
+     */
+    private static int fact(int i) {
+        return 0;
+    }
+}
