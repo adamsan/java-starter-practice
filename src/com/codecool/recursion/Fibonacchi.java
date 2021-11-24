68a34ff582ab0189e3b3dc1e3bafc5559928a259
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 11:29:34 +0100

    add task recursive fibonacchi calculaton

---
+++ src/com/codecool/recursion/Fibonacchi.java
@@ -0,0 +1,21 @@
+package com.codecool.recursion;
+
+public class Fibonacchi {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(fibo(i));
+        }
+
+    }
+
+    /**
+     * Calculates the i-th fibonacchi number.
+     * fibo(0) = 0
+     * fibo(1) = 1
+     * fibo(n) = fibo(n-1)+fibo(n-2)
+     */
+    private static int fibo(int i) {
+        return 0;
+    }
+}
