5bbe0a5ea34503def5c400013b7dfe48f06a1eae
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-24 11:10:07 +0100

    add recursive sum of array task

---
+++ src/com/codecool/recursion/SumArray.java
@@ -0,0 +1,14 @@
+package com.codecool.recursion;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 12, 11, 55};
+        System.out.println(sumArray(nums)); //88
+    }
+
+    private static int sumArray(int[] nums) {
+        //TODO: Calculate the sum of the nums array, use a recursive function.
+        //HINT: Use a private helper method with additional parameters.
+        return 0;
+    }
+}
