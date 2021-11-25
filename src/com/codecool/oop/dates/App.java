616b6547da9796e0b58518fce8d437a96473d05a
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-11-25 13:57:22 +0100

    add Calendar event task

---
+++ src/com/codecool/oop/dates/App.java
@@ -0,0 +1,14 @@
+package com.codecool.oop.dates;
+
+/**
+ * Write a calendar events program:
+ *  - data model: Event - contains the title of the event, the Date of the event, and the importance of the event [1,2,3].
+ *
+ *  - Create an interface with the following methods, then create and implementation for it:
+ *  - getFutureEvents - returns with a Map wit ha list of events for each day.
+ *  - findNextEvent() - returns with the next Event from now
+ *  - findNextImportantEvent - returns with the next important event from now.
+ *  - findEventsOnWeekends - returns with all the events for next weekend.
+ */
+public class App {
+}
