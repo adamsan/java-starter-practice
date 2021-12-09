cf2d2ceed66493c3d48ab49535cfe1e098e52180
Adam Szentirmai-Schon <adam.szentirmai-schon@codecool.com>
2021-12-09 15:32:24 +0100

    add GameFactory task

---
+++ src/com/codecool/oop/gamefactory/GameFactoryApp.java
@@ -0,0 +1,17 @@
+package com.codecool.oop.gamefactory;
+/**
+ * Create a class hierarchy for Games. A Game can be a CardGame, a BoardGame, a ComputerGame.
+ * A GameFactory creates Games, so for each Game type, create a corresponding factory - for example: CardGameFactory
+ * Also create a MixedGameFactory, which randomly creates a different type of Game, when it's create method is called.
+ * Create a GameFactoryCreator class, which builds a GameFacory, for each possible types of games.
+ *
+ * */
+public class GameFactoryApp {
+    public static void main(String[] args) {
+//        GameFactoryCreator gameFactoryCreator = new GameFactoryCreator();
+//        GameFactory gameFactory = gameFactoryCreator.create(GameType.MIXED);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(gameFactory.create());
+//        }
+    }
+}
